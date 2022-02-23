44d04019f2cd2709d3012c18d61cea183ba1bb58
gorr2012 <sergey_gornost@mail.ru>
2022-02-23 19:06:27 +0300

    add insert queries

---
+++ scripts/inserts/course.sql
@@ -0,0 +1,5 @@
+insert into course (id, name, start_date, end_date) values (1, 'Javascript', '2022-01-16T04:08:16.000Z', '2022-07-31T18:41:46.000Z');
+insert into course (id, name, start_date, end_date) values (2, 'NodeJS', '2022-02-10T12:02:00.000Z', '2023-02-12T01:47:08.000Z');
+insert into course (id, name, start_date, end_date) values (3, 'Typescript', '2022-01-31T18:30:08.000Z', '2022-03-05T01:31:25.000Z');
+insert into course (id, name, start_date, end_date) values (4, 'SQL', '2022-02-21T04:14:17.000Z', '2022-11-29T08:20:16.000Z');
+insert into course (id, name, start_date, end_date) values (5, 'Postgres', '2021-11-30T21:09:29.000Z', '2022-11-19T00:07:08.000Z');
